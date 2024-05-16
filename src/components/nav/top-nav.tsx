a77a42cd62c71d35501e839cd4d06723075983a3
damianoconnor04 <damianoconnor04@gmail.com>
2024-05-16 14:30:01 -0400

    pass session to AuthBtns fix

--- src/components/nav/top-nav.tsx
+++ src/components/nav/top-nav.tsx
@@ -1,9 +1,13 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import DesktopNav from "./desktop-nav";
 import AuthBtns from "./auth-btns";
+import { useSession } from "next-auth/react";
 
 export default function TopNav() {
+    const { data: session } = useSession();
+
     return (
         <header className="fixed left-0 top-0 flex h-16 w-full items-center">
             <div className="container relative flex mx-auto max-w-screen-xl items-center justify-between">
@@ -11,7 +15,7 @@ export default function TopNav() {
                 <nav className="absolute left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 sm:block">
                     <DesktopNav />
                 </nav>
-                <AuthBtns />
+                <AuthBtns session={session} />
             </div>
         </header>
     )
